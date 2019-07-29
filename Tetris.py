2a8d5198ce45ae3733bce81c7ad887b65bb90e14
klforthwind <kl.forthwind@gmail.com>
2019-07-29 06:20:41 +0000

    convert more code to python

--- Tetris.py
+++ Tetris.py
@@ -5,4 +5,23 @@ class Genome:
     inputNodes = 256
     outputNodes = 7
     neuralNet = np.random.random(outputNodes, inputNodes)
-    
+
+populationSize = 50
+generation = 0
+genomes = []
+currentGenome = -1
+mutationRate = 0.05
+mutationStep = 0.2
+
+archive = {
+    "popSize": 0,
+    "currGen": 0,
+    "elites": [],
+    "genomes": []
+}
+
+archive["popSize"] = populationSize
+for x in range(populationSize):
+    genomes.append(Genome())
+
+currentGenome+=1
