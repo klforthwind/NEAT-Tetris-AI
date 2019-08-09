63f76ca4a7e21bd0a65f2a3d1f0525342d7bff21
klforthwind <kl.forthwind@gmail.com>
2019-08-09 07:03:54 +0000

    add more functions to switch data class

--- switchdata.py
+++ switchdata.py
@@ -6,4 +6,18 @@ import time
 class SwitchData
 
     def __init__(self):
-        print('Starting Switch Data!')
+        self.cap = cv2.VideoCapture(0)
+        print(self.cap.set(3,1280))
+
+    def update(self):
+        # Capture frame-by-frame
+		self.ret, self.frame = cap.read()
+        
+    def show(self):
+        cv2.imshow('frame', self.frame)
+
+    def specialAnalysis(self):
+        self.s = self.frame[507:587, 1051:1235]
+		self.s = cv2.cvtColor(self.s, cv2.COLOR_BGR2GRAY)
+        self.ret, self.s = cv2.threshold(self.s, 200, 255, cv2.THRESH_BINARY)
+        cv2.imshow('frame', self.s)
