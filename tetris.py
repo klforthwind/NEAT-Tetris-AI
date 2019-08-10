6a6f78e3833baa76ae6918c604f176ed968189d3
klforthwind <kl.forthwind@gmail.com>
2019-08-10 01:36:16 -0500

    clear some unnecessary lines

--- tetris.py
+++ tetris.py
@@ -1,18 +1,16 @@
-import numpy.random as rand
+import numpy.random as rng
 import time
 from neat import NEAT
 from switchdata import SwitchData
 
 # Controlled randomness
-rand.seed(0)
+rng.seed(0)
 
 cap = SwitchData(0)
 t0 = time.time()
-i = 0
 cap.start()
 while True:
     cap.handleCapture()
-    i += 1
     if (cap.shouldQuit()):
         break
 cap.stop()
