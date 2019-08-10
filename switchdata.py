fbb56529c5e98cb0726b6026d85b21df39beeab6
klforthwind <kl.forthwind@gmail.com>
2019-08-10 01:29:45 -0500

    get rid of the cv2 import in tetris.py

--- switchdata.py
+++ switchdata.py
@@ -2,7 +2,7 @@ import threading
 import cv2
 
 class SwitchData:
-    def __init__(self, src=0, width=640, height=360):
+    def __init__(self, src=0, width=1280, height=720):
         self.src = src
         self.cap = cv2.VideoCapture(self.src, cv2.CAP_DSHOW)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
@@ -36,6 +36,14 @@ class SwitchData:
     def stop(self):
         self.started = False
         self.thread.join()
+        cv2.destroyAllWindows()
+    
+    def handleCapture(self):
+        _, frame = self.read()
+        cv2.imshow('Frame', frame)
+
+    def shouldQuit(self):
+        return cv2.waitKey(1) & 0xFF == ord('q')
 
     def __exit__(self, exec_type, exc_value, traceback):
         self.cap.release()
--- tetris.py
+++ tetris.py
@@ -1,6 +1,4 @@
-import numpy as np
 import numpy.random as rand
-import cv2
 import time
 from neat import NEAT
 from switchdata import SwitchData
@@ -13,13 +11,11 @@ t0 = time.time()
 i = 0
 cap.start()
 while True:
-    _, frame = cap.read()
-    cv2.imshow('Frame', frame)
+    cap.handleCapture()
     i += 1
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if (cap.shouldQuit()):
         break
 cap.stop()
-cv2.destroyAllWindows()
 
 
     
