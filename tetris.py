9c85e9ba12303fd314f67448c10600e039957613
klforthwind <kl.forthwind@gmail.com>
2019-08-10 02:00:37 +0000

    add more stuff to main file

--- tetris.py
+++ tetris.py
@@ -2,12 +2,15 @@ import numpy as np
 import random 
 import numpy.random as rand
 from neat import NEAT
-from shape import Shape
+from switchdata import SwitchData()
 
 # Controlled randomness
 rand.seed(0)
 
 neat = NEAT()
+data = SwitchData()
+while(true):
+    data.update()
     
 
 
