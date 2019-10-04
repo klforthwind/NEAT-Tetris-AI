3a610e7e8bb37e1af725e0473c97e0bf33a3e33d
Kevin La Casse <kl.forthwind@gmail.com>
2019-10-04 07:53:48 -0500

    begin updating tests

--- unit_tests.py
+++ unit_tests.py
@@ -1,9 +1,16 @@
+from numpy import uint8
+import datahandler
 import unittest
 
 class TestDataHandler(unittest.TestCase):
+	def __init__(self, *args, **kwargs):
+        super(TestingClass, self).__init__(*args, **kwargs)
+		self.test_case = np.array([[1, 0, 0], [1, 1, 0]], uint8)
+		self.dh = DataHandler()
 
     def test_getHeights(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+		print(self.dh.getHeights(self.test_case))
+        self.assertEqual(self.dh.getHeights(self.test_case), np.array([2,1,0]), "Should be 6")
 
     def test_getQueueBlocks(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
