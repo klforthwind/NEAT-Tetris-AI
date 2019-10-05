ec81b9b16f4db63d0c7fcaa853d0119d3c278c78
klforthwind <kl.forthwind@gmail.com>
2019-10-05 06:01:19 +0000

    make more test progress

--- datahandler.py
+++ datahandler.py
@@ -8,9 +8,9 @@ class DataHandler:
 
     # Returns heights of the board, height is relative from distance between bottom and heighest filled tile (0 is empty column)
     def getHeights(self, board):
-        heights = np.argmax(board, axis=0)
-        heights = np.where(heights > 0, heights, 20)
-        return np.subtract(20, heights)
+        heights = np.argmax(board, axis=0)              # Get the highest filled tile in each column
+        heights = np.where(heights > 0, heights, 20)    # Replace heights that are 0 with 20 (bottom of board)
+        return np.subtract(20, heights)                 # Subtract each value from 20, to flip the board to get heights correctly
 
     # Returns a list of blocks in the Queue
     def getQueueBlocks(self, queueArr):
--- unit_tests.py
+++ unit_tests.py
@@ -10,16 +10,16 @@ class TestDataHandler(unittest.TestCase):
         super(TestDataHandler, self).__init__(*args, **kwargs)
         self.dh = DataHandler()
 
+    def equality(self, expected, actual):
+        self.assertEqual( aequal(expected, actual), True, 
+            "Should be {}, but was {}".format(expected, actual))
+
     def test_getHeights(self):
-        self.assertEqual(
-            aequal(self.dh.getHeights(test1), test1Compare), True, 
-            "Should be {}, but was {}".format(test1Compare, self.dh.getHeights(test1)))
-        self.assertEqual(
-            aequal(self.dh.getHeights(test2), test2Compare), True, 
-            "Should be {}, but was {}".format(test2Compare, self.dh.getHeights(test2)))
+        self.equality(test1Compare, self.dh.getHeights(test1))
+        self.equality(test2Compare, self.dh.getHeights(test2))
 
     def test_getQueueBlocks(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+        self.equality(test3Compare, self.dh.getQueueBlocks(test3))
     
     def test_zero(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
