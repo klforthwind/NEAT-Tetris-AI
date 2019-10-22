b08ee4a961ac07ba10f25d1675ce7925981c7a0b
klforthwind <kl.forthwind@gmail.com>
2019-10-22 10:16:44 -0500

    indent comments

--- tetris.py
+++ tetris.py
@@ -6,34 +6,34 @@ import numpy.random as rng
 from neat import NEAT
 from time import time
 
-rng.seed(666)               # Controlled randomness, although Tetris blocks are random :P
+rng.seed(666)                       # Controlled randomness, although Tetris blocks are random :P
 
-t0 = time()                 # Get a relative point of time
+t0 = time()                         # Get a relative point of time
 
-capture = SwitchData()      # Connect to the Switch Capture
-capture.start()             # Run the Switch Capture asynchronously
+capture = SwitchData()              # Connect to the Switch Capture
+capture.start()                     # Run the Switch Capture asynchronously
 
-populationSize = 50         # Set the population size
-neat = NEAT(populationSize) # Begin our population
+populationSize = 50                 # Set the population size
+neat = NEAT(populationSize)         # Begin our population
 
 # Check to see if there is save data for the neural network to return to
-fileManager = FileManager() # Initialize a file manager to create / read files
+fileManager = FileManager()         # Initialize a file manager to create / read files
 loadable = fileManager.loadable()   # Get a tuple of (Boolean, Generation) as to whether the genome files are loadable
-if loadable[0]:             # Check to see if genomes files exist
+if loadable[0]:                     # Check to see if genomes files exist
     neat.repopulate(loadable[1])    # Repopulate with the oldest existent genome generation
 else:
-    neat.createPopulation() # Create a new population since none exist
+    neat.createPopulation()         # Create a new population since none exist
 
-port = "COM3"               # Set port for emulation
-emulator = Emulator(port)   # Create emulation on the specific port
+port = "COM3"                       # Set port for emulation
+emulator = Emulator(port)           # Create emulation on the specific port
 
-while True:                 # Main code loop :)
+while True:                         # Main code loop :)
     
-    if (capture.shouldQuit()):  #Check to see if the program should end (if we pressed q)
-        emulator.stop_input()   # Stop any inputs to the emulator
-        break                   # Break out of the while loop
+    if (capture.shouldQuit()):      #Check to see if the program should end (if we pressed q)
+        emulator.stop_input()       # Stop any inputs to the emulator
+        break                       # Break out of the while loop
 
-    capture.processCapture()    # Make and display the board, hold, and queue, and game frame
+    capture.processCapture()        # Make and display the board, hold, and queue, and game frame
 
     # Check to see if we should press A (genome over, and it won't go to next genome),
     # or check to see if genome is dead to press A
@@ -55,9 +55,9 @@ while True:                 # Main code loop :)
         else:
             emulator.stop_input()   # Send signals to stop the emulator from sending button data
 
-        neat.printFitness() # Print the fitness
+        neat.printFitness()         # Print the fitness
     else:
-        emulator.stop_input()   # Send signals to stop the emulator from sending button data
+        emulator.stop_input()       # Send signals to stop the emulator from sending button data
 
-capture.stop()      # Stop the capture thread
-emulator.close()    # Stop the emulator
+capture.stop()                      # Stop the capture thread
+emulator.close()                    # Stop the emulator
