481224ffc368f932fe26168b3a77e654d38d6522
klforthwind <kl.forthwind@gmail.com>
2019-11-05 08:34:55 -0600

    simplify filemanager.py

--- filemanager.py
+++ filemanager.py
@@ -7,11 +7,7 @@ class FileManager:
 
     def loadable(self):
         gen = 0
-        if isfile('data/0'+self.suffix):
-            while(isfile('data/'+str(gen)+self.suffix)):
-                hasData = isfile('data/'+str(gen)+self.suffix)
-                if hasData:
-                    gen += 1
-            return (True, gen - 1)
-        else:
-            return (False, 0)
+        isLoadable = isfile('data/0'+self.suffix)
+        while(isfile('data/'+str(gen)+self.suffix)):
+            gen += 1
+        return (isLoadable, gen - 1)
