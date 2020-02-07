4648399d1f50f319e78cf34a627f4fb620a4c917
klforthwind <kl.forthwind@gmail.com>
2020-02-07 17:51:35 -0600

    update tetris game

--- tetris.py
+++ tetris.py
@@ -4,11 +4,10 @@ from random import seed
 class Tetris:
 
     def __init__(self):
-        self.board = ([99] + [0] * 10 + [99]) * 20 + [99] * 24
+        self.board = ([99,99] + [0] * 10 + [99,99]) * 20 + [99] * 28
         self.queue = [0] * 48
         self.hold = [0] * 8
 
-        self.shape_ids = [None, "I", "J", "L", "O", "S", "T", "Z"]
         self.shapes = {
 	        "I": [[0,0,0,0], [1,1,1,1], [0,0,0,0], [0,0,0,0]],
 	        "J": [[2,0,0], [2,2,2], [0,0,0]],
@@ -16,8 +15,8 @@ class Tetris:
 	        "O": [[4,4], [4,4]],
 	        "S": [[0,5,5], [5,5,0], [0,0,0]],
 	        "T": [[0,6,0], [6,6,6], [0,0,0]],
-	        "Z": [[7,7,0], [0,7,7], [0,0,0]]
-        }
+	        "Z": [[7,7,0], [0,7,7], [0,0,0]]}
+        self.shape_ids = [None] + list(self.shapes.keys())
 
         self.jlstz_kicks = {
             "0>R": [(0, 0), (-1, 0),(-1,-1),(0, 2), (-1, 2)],
@@ -27,8 +26,7 @@ class Tetris:
             "2>L": [(0, 0), (1, 0), (1, -1),(0, 2), (1, -2)],
             "L>2": [(0, 0), (-1, 0),(-1, 1),(0, -2),(-1,-2)],
             "L>0": [(0, 0), (-1, 0),(-1, 1),(0, -2),(-1,-2)],
-            "0>L": [(0, 0), (1, 0), (1, -1),(0, 2), (1, 2)]
-        }
+            "0>L": [(0, 0), (1, 0), (1, -1),(0, 2), (1, 2)]}
 
         self.i_kicks = {
             "0>R": [(0, 0), (-2, 0),(1, 0), (-2, 1),(1, -2)],
@@ -38,54 +36,35 @@ class Tetris:
             "2>L": [(0, 0), (2, 0), (-1,0), (2, -1),(-1, 2)],
             "L>2": [(0, 0), (-2, 0),(1, 0), (-2, 1),(1,-2)],
             "L>0": [(0, 0), (1, 0), (-2,0), (1, 2), (-2,-1)],
-            "0>L": [(0, 0), (-1, 0),(2, 0), (-1,-2),(2, 1)]
-        }
+            "0>L": [(0, 0), (-1, 0),(2, 0), (-1,-2),(2, 1)]}
 
         self.current = {"x":0, "y":0, "r":0, "key":None, "shape":None}
         self.game_over = False
         self.has_held = False
-        self.block_count = 0
+        self.block_seed = 42
         self.score = 0
         self.bag = []
-        self.shape_actions = 0
 
         self.increase_bag()
         self.next_block()
 
-    def print_data(self):
-        data = self.queue + self.hold
-        r = 0
-        print("\n\n\n")
-        color = ["\033[96m","\033[31m","\033[37m","\033[91m","\033[33m","\033[32m","\033[35m","\033[31m",]
-        for row in range(20):
-            side_data = ""
-            if row % 3 != 2:
-                side_data = data[r * 4 : (r + 1) * 4]
-                r += 1
-            dt = [str(x) for x in self.board[row * 12 + 1: (row + 1) * 12 - 1]]
-            dt = [color[int(x)] + x for x in dt]
-            print("\033[96m[" + "\033[96m, ".join(dt) + "\033[96m]", end= "       ")
-            print(side_data)
-        print("\n")
+    def handle_i_piece(self, shape):
+        if max(shape[0 : 4]) == 0:
+            shape[0 : 4] = shape[4 : 8]
+            shape[4 : 8] = [0] * 4
 
     def set_hold(self, key):
         self.hold = self.get_shape_data(key)[0 : 8]
-        if max(self.hold[0 : 4]) == 0:
-            self.hold[0 : 4] = self.hold[4 : 8]
-            self.hold[4 : 8] = [0] * 4
+        self.handle_i_piece(self.hold)
 
     get_hold = lambda self : self.shape_ids[max(self.hold)]
 
     def get_shape_data(self, key):
-        block = self.shapes[key]
         lst = []
-        for layer in block:
-            if len(layer) == 2:
-                lst.append(0)
-            for val in layer:
-                lst.append(val)
-            if len(layer) != 4:
-                lst.append(0)
+        for layer in self.shapes[key]:
+            lst += [0] if len(layer) == 2 else []
+            lst += layer
+            lst += [0] if len(layer) != 4 else []
         return lst
 
     def set_queue(self):
@@ -93,37 +72,31 @@ class Tetris:
             self.increase_bag()
         for s in range(6):
             block_data = self.get_shape_data(self.bag[s])
-            if max(block_data[0 : 4]) == 0:
-                block_data[0 : 4] = block_data[4 : 8]
-                block_data[4 : 8] = [0] * 4
+            self.handle_i_piece(block_data)
             self.queue[s * 8 : (s + 1) * 8] = block_data[0 : 8]
 
-    def board_pos(self, x, y):
-        return (x + 1) + y * 12
+    board_pos = lambda self, x, y : (x + 2) + y * 14
 
-    def queue_pos(self, x ,y):
-        return x + y * 4
+    queue_pos = lambda self, x, y : x + y * 4
 
     def increase_bag(self):
         keys = list(self.shapes.keys())
-        seed(self.block_count + 9)
-        self.block_count += 7
+        seed(self.block_seed)
+        self.block_seed += 7
         shuffle(keys)
-        for s in keys:
-            self.bag.append(s)
+        self.bag += keys
 
     def next_block(self):
         self.score += 1
-        self.shape_actions = 0
         next_key = self.bag.pop(0)
         self.current = {
-            "x": int(5) - int((len(self.shapes[next_key]) + 1) / 2), 
+            "x": 5 - int((len(self.shapes[next_key]) + 1) / 2), 
             "y": 0, "r": 0, "key": next_key, 
             "shape": self.shapes[next_key] }
         self.set_queue()
         self.apply_shape()
 
-    def mask_shape(self, being_applied):
+    def _mask_shape(self, being_applied):
         shape = self.current["shape"]
         for y in range(len(shape)):
             for x in range(len(shape[0])):
@@ -133,10 +106,10 @@ class Tetris:
                     self.board[loc] = val if being_applied else 0
 
     def apply_shape(self):
-        self.mask_shape(True)
+        self._mask_shape(True)
 
     def remove_shape(self):
-        self.mask_shape(False)
+        self._mask_shape(False)
     
     def exists_collision(self):
         shape = self.current["shape"]
@@ -174,6 +147,10 @@ class Tetris:
     reverse = lambda self, lst : lst[::-1]
 
     transpose = lambda self, lst : list(zip(*lst))
+
+    def rotate(self, shape, going_left):
+        return (self.transpose(self.reverse(shape)),
+                self.reverse(self.transpose(shape)))[going_left]
     
     def update(self):
         self.remove_shape()
@@ -181,20 +158,18 @@ class Tetris:
         new_block = self.exists_collision()
         self.current["y"] -= 1 if new_block else 0
         self.apply_shape()
-        for y in range(20):
-            if min(self.board[y * 12  + 1: (y + 1) * 12 - 1]) > 0:
-                self.score += 4
-                for a in range(12):
-                    self.board.pop(y * 12)
-                self.board = [99] + [0] * 10 + [99] + self.board
         if new_block:
+            for y in range(20):
+                if min(self.board[y * 14 + 2: (y + 1) * 14 - 2]) > 0:
+                    self.score += 4
+                    for a in range(14):
+                        self.board.pop(y * 14)
+                    self.board = [99,99] + [0] * 10 + [99,99] + self.board
             self.has_held = False
             last_count = sum(self.board)
             if max(self.board[4:8]) != 0:
                 self.game_over = True
             self.next_block()
-        if self.shape_actions > 1000:
-            self.try_hard_drop()
 
     def try_rot(self, going_left):
         choices = ["0>L","R>0","2>R","L>2"] if going_left else ["0>R","R>2","2>L","L>0"]
@@ -203,9 +178,9 @@ class Tetris:
             self.remove_shape()
             choice = choices[self.current["r"] % 4]
             self.current["r"] += 3 if going_left else 1
+            self.current["r"] %= 4
             old = dict(self.current)
-            self.current["shape"] = (self.transpose(self.reverse(self.current["shape"])),
-                self.reverse(self.transpose(self.current["shape"])))[going_left]
+            self.current["shape"] = self.rotate(self.current["shape"], going_left)
             checks = self.i_kicks[choice] if self.current["key"] == "I" else self.jlstz_kicks[choice]
             for check in checks:
                 self.current["x"] = old["x"] + check[0]
@@ -245,7 +220,6 @@ class Tetris:
     def try_hard_drop(self):
         for i in range(20):
             self.try_soft_drop()
-        self.update()
 
     def try_hold(self):
         if not self.has_held:
@@ -261,42 +235,77 @@ class Tetris:
                 self.current["shape"] = self.shapes[old_held]
             self.has_held = True
 
-    def get_hold_and_q_blocks(self):
-        return (self.hold, self.queue[0:8], self.queue[8:16])
-
-    def get_input_nodes(self):
-        lst = [0] * 300
-        s_x = self.current["x"] + self.left_most()
-        s_y = self.current["y"]
-        for y in range(20):
-            y_deviated = s_y + y - 10
-            if not (0<=y_deviated<=19):
-                continue
-            for x in range(15):
-                x_deviated = s_y + x - 6
-                if not (0<=x_deviated<=9):
-                    continue
-                loc = self.board_pos(x_deviated, y_deviated)
-                if self.board[loc]:
-                    lst[x + y * 15] = 1
-                else:
-                    lst[x + y * 15] = -1
-        return list(lst)
-
-    def perform_movement(self, btns):
-        if btns[0] > 0:
+    def perform_movement(self, move):
+        if move == "RL":
             self.try_rot_left()
-        elif btns[1] > 0:
+        elif move == "RR":
             self.try_rot_right()
-        elif btns[2] > 0:
+        elif move == "L":
             self.try_left()
-        elif btns[3] > 0:
+        elif move == "R":
             self.try_right()
-        elif btns[4] > 0:
+        elif move == "SD":
             self.try_soft_drop()
-        elif btns[5] > 0:
+        elif move == "HD":
             self.try_hard_drop()
-        elif btns[6] > 0:
+            self.update()
+        elif move == "H":
             self.try_hold()
 
-        self.shape_actions += 1
+##################################################
+# AI Code
+
+    def get_game_state(self):
+        lst = [0] * 200
+        for y in range(20):
+            for x in range(10):
+                loc = self.board_pos(x, y)
+                if self.board[loc]:
+                    lst[x + y * 10] = 1
+        curr_piece = self.get_shape_data(self.current["key"])[0 : 8]
+        self.handle_i_piece(curr_piece)
+        lst = lst + self.hold + curr_piece + self.queue[:-8]
+        return list(lst)
+    
+    def get_next_game_state(self):
+        lst = [0] * 200
+        for y in range(20):
+            for x in range(10):
+                loc = self.board_pos(x, y)
+                if self.board[loc]:
+                    lst[x + y * 10] = 1
+        lst = lst + self.hold + self.queue
+        return list(lst)
+
+    def can_place(self):
+        shape = self.current["shape"]
+        for y in range(len(shape)):
+            for x in range(len(shape[0])):
+                if shape[y][x]:
+                    loc = self.board_pos(self.current["x"] + x, self.current["y"] + y + 1)
+                    if self.board[loc] != 0:
+                        return True
+        return False
+
+    def get_height(self):
+        total = 0
+        for x in range(10):
+            for y in range(20):
+                if self.board[self.board_pos(x, y)]:
+                    total += 20 - y
+                    break
+        return total
+
+    def get_holes(self):
+        total = 0
+        for x in range(10):
+            for y in range(20):
+                if self.board[self.board_pos(x, y)]:
+                    total += 1
+        return self.get_height() - total
+
+    def get_completed_lines(self):
+        lines = 0
+        for row in range(20):
+            lines += min(self.board[row * 14 + 2 : row * 14 + 12]) != 0
+        return lines
