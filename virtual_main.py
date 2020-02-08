9cb44b978ce386bc91f8a273252791158103faea
klforthwind <kl.forthwind@gmail.com>
2020-02-07 22:49:17 -0600

    clean up main

--- virtual_main.py
+++ virtual_main.py
@@ -12,55 +12,26 @@ if __name__ == "__main__":
     neat = NEAT(population_size, loadable)
 
     node_manager = NodeManager()
-    # view = AsciimaticView()
+    view = AsciimaticView()
 
     max_score = 0
-    scores = []
-
     tetris = Tetris()
 
-    for x in range(1):
-        tetris = Tetris()
-        node_list = []
-        
-        while not tetris.game_over:
-            best_node = node_manager.analyze(tetris)
-            node_list.append(best_node.hexcode)
-
-            moves = best_node.movement 
-            for move in moves:
-                tetris.perform_movement(move)
-                # view.refresh(tetris.board, moves, [move])
-
-            tetris.update()
-        
     while True:
-        # print("Max score:", max_score)
-        # print("Scores: ",end = "")
-        # print(scores)
 
         if tetris.game_over:
             score = tetris.score
-            max_score = score if score > max_score else max_score
+            max_score = max(score, max_score)
             neat.genomes[neat.current_genome].fitness = score
-            scores.append(score)
             print("Max score:", max_score)
-            print("Scores: ",end = "")
-            print(scores)
             tetris = Tetris()
             neat.loop()
-            if neat.current_genome == 0 or len(scores) > 32:
-                scores = []
 
-        for i in range(8):
-            input_nodes = tetris.get_input_nodes()
-            block, rotation = tetris.current["key"], tetris.current["r"]
-            hold, q1 = tetris.get_hold_and_q1()
+        best_node = node_manager.analyze(tetris, neat.get_current_nn())
 
-            btn_array = neat.get_movements(block, rotation, hold, q1, input_nodes)
-
-            tetris.perform_movement(btn_array)
+        moves = best_node.movement 
+        for move in moves:
+            tetris.perform_movement(move)
+            view.refresh(tetris.board, moves, [move])
 
         tetris.update()
-        # clear()
-        # tetris.print_data()
